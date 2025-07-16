37dc345dcda76779a498a2ad29d8a43fe0806325
Mills-44 <mylesbrodsky44@gmail.com>
2025-07-16 16:59:58 -0500

    Delete Meridiem directory

--- Meridiem/lib/util.lua
+++
@@ -1,8 +0,0 @@
----Registers a list of items in a custom order
----@param items table
----@param path string
-function MERIDIEM.file_loader(items, path)
-  for i = 1, #items do
-    SMODS.load_file(path .. "/" .. items[i] .. ".lua")()
-  end
-end
