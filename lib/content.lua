4885c82cd559af453f638f12a0b1189171046b7c
Mills-44 <mylesbrodsky44@gmail.com>
2025-07-19 16:55:51 -0500

    Add files via upload

--- lib/content.lua
+++ lib/content.lua
@@ -3,18 +3,18 @@ MERIDIEM.config = SMODS.current_mod.config
 MERIDIEM.JOKERS = {
    "a_fools_camera",
    "backstage",
-   "below_the_rainbow",
-   "druid",
+   --"below_the_rainbow",
+   --"druid",
    "funky_joker",
    "megapolis",
    "moonlight",
    "lord_of_chips",
    "ouroboros",
-   "wire_man"
+   --"wire_man"
 }
 
 MERIDIEM.STICKERS = {
-   "rooted"
+   --"rooted"
 }
 
 MERIDIEM.EDITIONS = {
